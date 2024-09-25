8abfef7469e1e24894d9ae4f74b2ebe21c28b4ef
Marc Avante <avantecaballe@gmail.com>
2024-09-25 20:55:00 +0200

    Load triangles from json file.

--- sdl2_simple_example/sdl2_simple_example/main.cpp
+++ sdl2_simple_example/sdl2_simple_example/main.cpp
@@ -5,6 +5,10 @@
 #include <glm/glm.hpp>
 #include <SDL2/SDL_events.h>
 #include "MyWindow.h"
+#include <fstream>
+#include <iostream>
+#include "json/json.h"
+
 using namespace std;
 
 using hrclock = chrono::high_resolution_clock;
@@ -32,9 +36,9 @@ static void draw_triangle(const u8vec4& color, const vec3& center, double size) 
 	glEnd();
 }
 
-static void display_func() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	draw_triangle(u8vec4(255, 0, 0, 255), vec3(0.0, 0.0, 0.0), 0.5);
+static void display_func(Json::Value data) {
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	draw_triangle(u8vec4(data["color"][0].asInt(), data["color"][1].asInt(), data["color"][2].asInt(), data["color"][3].asInt()), vec3(data["center"][0].asDouble(), data["center"][1].asDouble(), data["center"][2].asDouble()), data["size"].asDouble());
 }
 
 static bool processEvents() {
@@ -50,17 +54,38 @@ static bool processEvents() {
 
 int main(int argc, char** argv) {
 	MyWindow window("SDL2 Simple Example", WINDOW_SIZE.x, WINDOW_SIZE.y);
+	ifstream file("data.json", ifstream::binary);
+	if (!file.is_open()) {
+		cerr << "Error opening file!" << endl;
+		return 1;
+	}
+
+	Json::Value jsonData;
+	Json::CharReaderBuilder readerBuilder;
+	string errs;
+
+	if (!Json::parseFromStream(readerBuilder, file, &jsonData, &errs)) {
+		cerr << "Error parsing the JSON file: " << errs << endl;
+		return 1;
+	}
+
+
 
 	init_openGL();
 
 	while (processEvents()) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		const auto t0 = hrclock::now();
-		display_func();
+		display_func(jsonData["triangle"]);
+		display_func(jsonData["triangle2"]);
+		display_func(jsonData["triangle3"]);
 		window.swapBuffers();
 		const auto t1 = hrclock::now();
 		const auto dt = t1 - t0;
 		if(dt<FRAME_DT) this_thread::sleep_for(FRAME_DT - dt);
 	}
+
+	file.close();
 
 	return 0;
 }
